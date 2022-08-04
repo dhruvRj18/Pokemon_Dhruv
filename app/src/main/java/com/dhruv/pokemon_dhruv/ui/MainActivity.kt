813dd86e8a77441bfd1b098ebf09d1be9d16feb7
dhruvRj18 <dhruv4218@gmail.com>
2022-08-04 20:01:31 +0100

    feat: added basic dependency injection skeleton

--- app/src/main/java/com/dhruv/pokemon_dhruv/ui/MainActivity.kt
+++ app/src/main/java/com/dhruv/pokemon_dhruv/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.dhruv.pokemon_dhruv
+package com.dhruv.pokemon_dhruv.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dhruv.pokemon_dhruv.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
