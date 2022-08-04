6cab2dd9e4f23776b8af23e1d67db7379133fef6
Dhruv Parmar <30573895+dhruvRj18@users.noreply.github.com>
2022-08-05 00:06:43 +0100

    Sct 4 design screens (#3)
    * chor: saving code
    
    * feat: stats screen added

--- app/src/main/java/com/dhruv/pokemon_dhruv/ui/MainActivity.kt
+++ app/src/main/java/com/dhruv/pokemon_dhruv/ui/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.dhruv.pokemon_dhruv.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.palette.graphics.Palette
 import com.dhruv.pokemon_dhruv.R
 import dagger.hilt.android.AndroidEntryPoint
+import java.lang.String
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //temp code for getting dominant color for detail screen
+        val bmp = BitmapFactory.decodeResource(this.resources,R.drawable.avatar).copy(Bitmap.Config.ARGB_8888,true)
+
+        Palette.from(bmp).generate{
+            it?.dominantSwatch?.rgb.let {
+                Log.d("color", String.format("#%06X", 0xFFFFFF and it!!))
+            }
+
+        }
     }
 }
